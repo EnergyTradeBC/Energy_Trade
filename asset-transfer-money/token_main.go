035de97050273df836b8fa055be4744daa2479f0
EnergyTradeBC <energy.tradebc@gmail.com>
2023-05-29 09:00:57 +0000

    token money update

---
+++ asset-transfer-money/token_main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-samples/token-utxo/chaincode-go/chaincode"
+)
+
+func main() {
+	tokenChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating token chaincode: %v", err)
+	}
+
+	if err := tokenChaincode.Start(); err != nil {
+		log.Panicf("Error starting token chaincode: %v", err)
+	}
+}
